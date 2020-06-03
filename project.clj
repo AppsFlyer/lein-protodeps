01de299af0edcd3a6efe95bd6b5a5f8c0cd2faec
Ronen Cohen <ronen.cohen@appsflyer.com>
2020-06-03 22:23:18 +0300

    init

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject lein-protobuf "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache License, Version 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :eval-in-leiningen true
+  :deploy-repositories [["releases"  {:url           "***REMOVED***/" :username :***REMOVED***
+                                      :password      :***REMOVED***
+                                      :sign-releases false}]
+                        ["snapshots"  {:url           "***REMOVED***" :username :***REMOVED***
+                                       :password      :***REMOVED***
+                                       :sign-releases false}]]
+  :repositories [["jitpack.io" "https://jitpack.io"]]
+  :dependencies [[clj-jgit "1.0.0-beta3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
