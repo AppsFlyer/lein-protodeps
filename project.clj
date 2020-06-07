f4b2167fcf7d5f7cdd246f790fffbc273e5adc85
Ronen Cohen <ronen.cohen@appsflyer.com>
2020-06-07 19:40:42 +0300

    wip

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-protobuf "0.1.0-SNAPSHOT"
+(defproject lein-protobuf "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"
