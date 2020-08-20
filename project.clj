a846455433a439a9cee1f9cafb5bd80e271c3110
Ronen Cohen <ronen.cohen@appsflyer.com>
2020-08-20 20:57:11 +0300

    Version 0.1.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-protodeps "0.1.13"
+(defproject lein-protodeps "0.1.14"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"
