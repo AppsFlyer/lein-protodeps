784b4b3a2c3dd9c5d00fe3b13adf82cb73b36edb
Ronen Cohen <ronen.cohen@appsflyer.com>
2021-05-24 14:09:31 +0300

    Version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-protodeps "1.0.0"
+(defproject lein-protodeps "1.0.1"
   :description "FIXME: write description"
   :url "***REMOVED***"
   :license {:name "Apache License, Version 2.0"
