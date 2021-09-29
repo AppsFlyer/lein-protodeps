b48e4cafd2a889df1dde1d9088212dd2d821462e
Ronen Cohen <ronen.cohen@appsflyer.com>
2021-09-29 16:12:03 +0300

    Version 1.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.appsflyer/lein-protodeps "1.0.2"
+(defproject com.appsflyer/lein-protodeps "1.0.3-SNAPSHOT"
   :description "Leiningen plugin for consuming and compiling protobuf schemas"
   :url "https://github.com/AppsFlyer/lein-protodeps"
   :license {:name "Apache License, Version 2.0"
